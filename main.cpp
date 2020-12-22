57506b4f562d3b0a7f6d47638656e82bc6314d94
mc-b <marcel.bernet@ch-open.ch>
2020-12-22 17:28:11 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,188 @@
+/** MQTT Publish von Sensordaten */
+#include "mbed.h"
+#include "HTS221Sensor.h"
+#include "OLEDDisplay.h"
+#include "Motor.h"
+#ifdef TARGET_NUCLEO_F303RE
+#include "BMP180Wrapper.h"
+#endif
+
+#include <MQTTClientMbedOs.h>
+#include <MQTTNetwork.h>
+#include <MQTTClient.h>
+#include <MQTTmbed.h> // Countdown
+
+// Sensoren wo Daten fuer Topics produzieren
+static DevI2C devI2c( MBED_CONF_IOTKIT_I2C_SDA, MBED_CONF_IOTKIT_I2C_SCL );
+#ifdef TARGET_NUCLEO_F303RE
+static BMP180Wrapper hum_temp( &devI2c );
+#else
+static HTS221Sensor hum_temp(&devI2c);
+#endif
+AnalogIn hallSensor( MBED_CONF_IOTKIT_HALL_SENSOR );
+DigitalIn button( MBED_CONF_IOTKIT_BUTTON1 );
+
+// Topic's
+char* topicTEMP = (char*) "iotkit/sensor";
+char* topicALERT = (char*) "iotkit/alert";
+char* topicBUTTON = (char*) "iotkit/button";
+// MQTT Brocker
+char* hostname = (char*) "cloud.tbz.ch";
+int port = 1883;
+// MQTT Message
+MQTT::Message message;
+// I/O Buffer
+char buf[100];
+
+// Klassifikation 
+char cls[3][10] = { "low", "middle", "high" };
+int type = 0;
+
+// UI
+OLEDDisplay oled( MBED_CONF_IOTKIT_OLED_RST, MBED_CONF_IOTKIT_OLED_SDA, MBED_CONF_IOTKIT_OLED_SCL );
+DigitalOut led1( MBED_CONF_IOTKIT_LED1 );
+DigitalOut alert( MBED_CONF_IOTKIT_LED3 );
+
+// Aktore(n)
+Motor m1( MBED_CONF_IOTKIT_MOTOR2_PWM, MBED_CONF_IOTKIT_MOTOR2_FWD, MBED_CONF_IOTKIT_MOTOR2_REV ); // PWM, Vorwaerts, Rueckwarts
+PwmOut speaker( MBED_CONF_IOTKIT_BUZZER );
+
+/** Hilfsfunktion zum Publizieren auf MQTT Broker */
+void publish( MQTTNetwork &mqttNetwork, MQTT::Client<MQTTNetwork, Countdown> &client, char* topic )
+{
+    led1 = 1;
+    printf("Connecting to %s:%d\r\n", hostname, port);
+    
+    int rc = mqttNetwork.connect(hostname, port);
+    if (rc != 0)
+        printf("rc from TCP connect is %d\r\n", rc);
+
+    MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
+    data.MQTTVersion = 3;
+    data.clientID.cstring = (char*) "mbed-sample";
+    data.username.cstring = (char*) "testuser";
+    data.password.cstring = (char*) "testpassword";
+    if ((rc = client.connect(data)) != 0)
+        printf("rc from MQTT connect is %d\r\n", rc);
+
+    MQTT::Message message;    
+    
+    oled.cursor( 2, 0 );
+    oled.printf( "Topi: %s\n", topic );
+    oled.cursor( 3, 0 );    
+    oled.printf( "Push: %s\n", buf );
+    message.qos = MQTT::QOS0;
+    message.retained = false;
+    message.dup = false;
+    message.payload = (void*) buf;
+    message.payloadlen = strlen(buf)+1;
+    client.publish( topic, message);  
+    
+    // Verbindung beenden, ansonsten ist nach 4x Schluss
+    if ((rc = client.disconnect()) != 0)
+        printf("rc from disconnect was %d\r\n", rc);
+
+    mqttNetwork.disconnect();
+    led1 = 0;
+}
+
+/** Hauptprogramm */
+int main()
+{
+    uint8_t id;
+    float temp, hum;
+    int encoder;
+    alert = 0;
+    
+    oled.clear();
+    oled.printf( "MQTTPublish\r\n" );
+    oled.printf( "host: %s:%s\r\n", hostname, port );
+
+    printf("\nConnecting to %s...\n", MBED_CONF_APP_WIFI_SSID);
+    oled.printf( "SSID: %s\r\n", MBED_CONF_APP_WIFI_SSID );
+    
+    // Connect to the network with the default networking interface
+    // if you use WiFi: see mbed_app.json for the credentials
+    WiFiInterface *wifi = WiFiInterface::get_default_instance();
+    if ( !wifi ) 
+    {
+        printf("ERROR: No WiFiInterface found.\n");
+        return -1;
+    }
+    printf("\nConnecting to %s...\n", MBED_CONF_APP_WIFI_SSID);
+    int ret = wifi->connect( MBED_CONF_APP_WIFI_SSID, MBED_CONF_APP_WIFI_PASSWORD, NSAPI_SECURITY_WPA_WPA2 );
+    if ( ret != 0 ) 
+    {
+        printf("\nConnection error: %d\n", ret);
+        return -1;
+    }    
+
+    // TCP/IP und MQTT initialisieren (muss in main erfolgen)
+    MQTTNetwork mqttNetwork( wifi );
+    MQTT::Client<MQTTNetwork, Countdown> client(mqttNetwork);
+    
+    /* Init all sensors with default params */
+    hum_temp.init(NULL);
+    hum_temp.enable();  
+    
+    while   ( 1 ) 
+    {
+        // Temperator und Luftfeuchtigkeit
+        hum_temp.read_id(&id);
+        hum_temp.get_temperature(&temp);
+        hum_temp.get_humidity(&hum);    
+        if  ( type == 0 )
+        {
+            temp -= 5.0f;
+            m1.speed( 0.0f );
+        }
+        else if  ( type == 2 )
+        {
+            temp += 5.0f;
+            m1.speed( 1.0f );
+        }
+        else
+        {
+            m1.speed( 0.75f );
+        }
+        sprintf( buf, "0x%X,%2.2f,%2.1f,%s", id, temp, hum, cls[type] ); 
+        type++;
+        if  ( type > 2 )
+            type = 0;       
+        publish( mqttNetwork, client, topicTEMP );
+        
+        // alert Tuer offen 
+        printf( "Hall %4.4f, alert %d\n", hallSensor.read(), alert.read() );
+        if  ( hallSensor.read() > 0.6f )
+        {
+            // nur einmal Melden!, bis Reset
+            if  ( alert.read() == 0 )
+            {
+                sprintf( buf, "alert: hall" );
+                message.payload = (void*) buf;
+                message.payloadlen = strlen(buf)+1;
+                publish( mqttNetwork, client, topicALERT );
+                alert = 1;
+            }
+            speaker.period( 1.0 / 3969.0 );      // 3969 = Tonfrequenz in Hz
+            speaker = 0.5f;
+            thread_sleep_for( 500 );
+            speaker.period( 1.0 / 2800.0 );
+            thread_sleep_for( 500 );
+        }
+        else
+        {
+            alert = 0;
+            speaker = 0.0f;
+        }
+
+        // Button (nur wenn gedrueckt)
+        if  ( button == 0 )
+        {
+            sprintf( buf, "ON" );
+            publish( mqttNetwork, client, topicBUTTON );
+        }
+
+        thread_sleep_for    ( 2000 );
+    }
+}
